a012e8c2020719fe884c2a215a25cac94d1f0691
Tuankien Truong <truo0059@algonquinlive.com>
2021-09-30 13:45:59 -0400

    Updated 'upload' method headers, added 3 parameters which are mongodb host, port and name

--- html/xlsxUpload.py
+++ html/xlsxUpload.py
@@ -5,15 +5,12 @@ import csv
 import sys, getopt, pprint
 
 
-def xlsx_upload(uploaded_file, path_to_file):
-    MONGODB_HOST = 'localhost'
-    MONGODB_PORT = 27017
-    DBS_NAME = 'donorschoose'
+def xlsx_upload(uploaded_file, path_to_file, DB_HOST, DB_PORT, DB_Name):
     COLLECTION_NAME = uploaded_file.split('.')[0]
 
-    connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
+    connection = MongoClient(DB_HOST, DB_PORT)
 
-    collection = connection[DBS_NAME][COLLECTION_NAME]
+    collection = connection[DB_Name][COLLECTION_NAME]
 
     excel_file = pd.read_excel(path_to_file + "/" + uploaded_file, engine='openpyxl', index_col=None)
 
